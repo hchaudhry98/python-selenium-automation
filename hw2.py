e39fbc9daa13fcb56a243e079d07ee6b505b2ec8
Hammad Chaudhry <hammadchaudhry@Hammads-MacBook-Pro.local>
2022-09-29 21:15:07 -0400

    Homework 2 Added

---
+++ hw2.py
@@ -0,0 +1,12 @@
+"""
+Amazon logo, search by XPATH, "//a[@class='a-link-nav-icon']"
+Email field, search by ID, “ap_email”
+Continue button, search by ID, "continue"
+Need help link, search by XPATH, "//a[@href='javascript:void(0)']"
+Forgot your password link, search by ID, "auth-fpp-link-bottom"
+Other issues with Sign-In, search by ID, "ap-other-signin-issues-link"
+Create your Amazon account, search by ID, "createAccountSubmit"
+Conditions of Use link, search by XPATH, "//a[text()='Conditions of Use']"
+Privacy Notice Link, search by XPATH, "//a[text()='Privacy Notice']"
+
+"""
